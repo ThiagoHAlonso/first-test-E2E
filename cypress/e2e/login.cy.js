6eaa14490e362a5d9e11e7b79567bbd483d1d662
Thiago Alonso <thiagohenriquealonso@gmail.com>
2026-04-11 20:35:57 -0300

    User update scenario added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -12,7 +12,10 @@ describe('Orange HRM Tests', () => {
     myInfoButton:'[href="/web/index.php/pim/viewMyDetails"]',
     fistNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
-    nickNameField:".oxd-input--active",
+    genericField:".oxd-input--active",
+    dateField:"[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    submitButton:"[type='submit']"
   }
 
   it.only('User Info Update - success', () => {
@@ -23,12 +26,22 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.loginButton).click()
 
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.dashboardGrid)
+    //cy.get(selectorList.dashboardGrid)
     cy.get(selectorList.myInfoButton)
-     //cy.visit('/pim/viewPersonalDetails/empNumber/7')
-    cy.get(selectorList.nickNameField).eq(4).type('NicknameTest') 
-
-
+    cy.visit('/pim/viewPersonalDetails/empNumber/7')
+    cy.get(selectorList.fistNameField).type('FirstNameTest')
+    cy.get(selectorList.lastNameField).type('LastNameTest')
+    cy.get(selectorList.genericField).eq(3).clear().type('NicknameTest') 
+    cy.get(selectorList.genericField).eq(4).clear().type('Employee')
+    cy.get(selectorList.genericField).eq(5).clear().type('OtherIdTest') 
+    cy.get(selectorList.genericField).eq(6).clear().type('Drivers License Number Test')
+    cy.get(selectorList.genericField).eq(7).clear().type('2026-07-07')
+    cy.get(selectorList.dateCloseButton).click() 
+    cy.get(selectorList.genericField).eq(8).clear().type('ssnNumberTest')  
+    cy.get(selectorList.genericField).eq(9).clear().type('sinNumberTest') 
+    cy.get(selectorList.dateCloseButton).eq(0).click()
+    cy.get('body').should('contain','Succesfully Updated')
+    cy.get('.oxd-toast-close')
   })
   it('Login - fail', () => {
     cy.visit('/auth/login')
